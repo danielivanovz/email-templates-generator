ecfac525e4ea875b382652ce5bb7b328804f44e9
Daniel Ivanov <daniel.ivanov@mavigex.com>
2022-06-22 01:45:49 +0200

    - added prettier;

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
-import { readdir, readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
-import mjml from 'mjml';
-import mustache from 'mustache'
+import { readdir, readFileSync, writeFileSync } from "fs"
+import { join } from "path"
+import mjml from "mjml"
+import mustache from "mustache"
 
-const dir = join(__dirname, '../src/templates');
-const out = join(__dirname, '../src/html/');
+const dir = join(__dirname, "../src/templates")
+const out = join(__dirname, "../src/html/")
 
-const HTML = '.html';
-const MJML = '.mjml';
+const HTML = ".html"
+const MJML = ".mjml"
 
 export function poshta() {
   return {
     convert: (dir: string, out: string) => {
-			return readdir(dir, (err, templates) => {
-				if (err) throw Error(`Error reading templates: ${err.message}`);
-	
-				templates.forEach((template) => {
-					console.info('Processing', template);
-	
-					const current = readFileSync(join(dir, template), 'utf8');
-					const html = out.concat(template.replace(MJML, HTML));
-	
-					writeFileSync(html, mjml(current).html);
-				});
-			})
-		},
-		render: (template: string, data: Record<string, unknown>) => {
-			return mustache.render(template, data);
-		}
-  };
+      return readdir(dir, (err, templates) => {
+        if (err) throw Error(`Error reading templates: ${err.message}`)
+
+        templates.forEach((template) => {
+          console.info("Processing", template)
+
+          const current = readFileSync(join(dir, template), "utf8")
+          const html = out.concat(template.replace(MJML, HTML))
+
+          writeFileSync(html, mjml(current).html)
+        })
+      })
+    },
+    render: (template: string, data: Record<string, unknown>) => {
+      return mustache.render(template, data)
+    },
+  }
 }
 
-export default poshta();
+export default poshta()
